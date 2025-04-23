f7ed19c4955b528e43f209a3bb63cbdee0e737de
Efrat <efratro123@gmail.com>
2025-04-23 18:32:56 +0300

    order by in model list

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -6,6 +6,7 @@ from odoo.exceptions import UserError
 class EstatePropertyOffer(models.Model):
 	_name = "estate.property.offer"
 	_description = "estate property offers"
+	_order = "price desc"
 
 	price = fields.Float()
 	status = fields.Selection(selection=[('Accepted', 'accepted'), ('Refused', 'refused')], copy=False)
