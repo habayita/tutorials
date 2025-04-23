a864f50ae34bfd5614094296b4a6a025fa8b816b
Efrat <efratro123@gmail.com>
2025-04-23 14:47:49 +0300

    python constrains

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,7 +1,8 @@
 from odoo import api, fields, models
 from datetime import date 
 from dateutil.relativedelta import relativedelta
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools.float_utils import float_compare, float_is_zero
 
 class EstateProperty(models.Model):
 	_name = "estate.property"
@@ -70,3 +71,12 @@ class EstateProperty(models.Model):
 			else:
 				raise UserError('Sold properties cannot be cancelled')
 			return True
+
+	@api.constrains('selling_price', 'expected_price')
+	def _check_selling_price(self):
+		for record in self:
+			if (
+				float_compare(record.selling_price, record.expected_price*0.9, precision_digits=2)==-1 
+				and not float_is_zero(record.selling_price, precision_digits=2)
+			):
+				raise ValidationError('The selling price cannot be less than 90% of the expected price')
