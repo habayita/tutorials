f647202669c8f59525558d992fca69ee89eccb94
Efrat <efratro123@gmail.com>
2025-07-27 20:22:24 +0300

    .

--- estate/models/estate_property_tag.py
+++ estate/models/estate_property_tag.py
@@ -6,6 +6,7 @@ class EstatePropertyTag(models.Model):
 	_order = "name"
 
 	name = fields.Char(required=True)
+	color = fields.Integer()
 
 	_sql_constraints = [
         ('unique_tag_name', 'UNIQUE(name)', 'The tag name nust be unique.')
